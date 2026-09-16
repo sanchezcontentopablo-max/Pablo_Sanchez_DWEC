be49f913bdb8a467a09048827b5777e5f136256d
LAPTOP-TLI1SARF\pablo <sanchezcontento.pablo@jgvaldemora.org>
2026-09-16 13:21:49 +0200

    Modo noche punto extra

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+
 const boton = document.getElementById("boton");
 const aviso = document.getElementById("aviso");
 
 let contador = 0;
 
-boton.addEventListener("click", function () {
-    contador++;
-    aviso.textContent = "Has pulsado el botón " + contador + " veces";
-});
+if (boton && aviso) {
+    boton.addEventListener("click", function () {
+        contador++;
+        aviso.textContent = "Has pulsado el botón " + contador + " veces";
+    });
+}
+
+const botonModo = document.getElementById("modonoche");
+
+if (botonModo) {
+    botonModo.addEventListener("click", function () {
+        document.body.classList.toggle("tema-claro");
+
+        if (document.body.classList.contains("tema-claro")) {
+            botonModo.textContent = "🌙";
+        } else {
+            botonModo.textContent = "☀️";
+        }
+    });
+}
