87a4efd4c78d2586e4c6f686954a36b481e0f0b8
LAPTOP-TLI1SARF\pablo <sanchezcontento.pablo@jgvaldemora.org>
2026-09-16 13:07:03 +0200

    Funcion para el boton

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");
+const boton = document.getElementById("boton");
+const aviso = document.getElementById("aviso");
+
+let contador = 0;
+
+boton.addEventListener("click", function () {
+    contador++;
+    aviso.textContent = "Has pulsado el botón " + contador + " veces";
+});
